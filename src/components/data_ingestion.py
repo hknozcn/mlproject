fb52f3952a6f151991c8ffdf66cf24662cb29d4b
Hakan <a2251@A2251s-MacBook-Pro.local>
2025-03-17 12:12:28 +0100

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,9 +3,17 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
+import pickle
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformationConfig
+from src.utils import save_object
+
+
+from src.components.data_transformation import DataTransformation
+
+
 
 @dataclass
 class DataIngestionConfig:
@@ -39,5 +47,8 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+
         
